40d81b04bee927c7a8ba43164de467cb85e5566a
iosonodipe <dipecod@gmail.com>
2024-02-07 20:33:46 +0100

    todolist completa

--- M1/s3/g3/JS/script.js
+++ M1/s3/g3/JS/script.js
@@ -1,24 +1,32 @@
-const bottoneAggiungi = document.querySelector("#aggiungiTask");
-let testoTask = document.querySelector("#task");
-let elenco = document.querySelector("#listaTask");
-let nuovaTask = document.createElement("li");
-let bottoneRimuovi = document.createElement("button");
+// creo i selettori del: testo in input, bottone aggiungi e lista ul
+const testo = document.querySelector("#testo");
+const aggiungi = document.querySelector("#aggiungi");
+const lista = document.querySelector("#lista");
 
-bottoneAggiungi.addEventListener("click", addTask);
-bottoneRimuovi.addEventListener("click", removeTask);
+// creo evento che aspetta il click del bottone aggiungi ed esegue la funzione aggiungi task
+aggiungi.addEventListener("click", aggiungiTask);
+// testo.addEventListener("keyup", aggiungiTask(e));
 
-console.dir(bottoneRimuovi);
+function aggiungiTask() {
+  if (testo.value) {
+    let li = document.createElement("li"); //creo li
+    lista.append(li); //appendo a UL > li
+    li.innerText = testo.value; //assegno a li il testo inserito nell'inpunt
 
-function addTask() {
-  if (testoTask.value) {
-    elenco.append(nuovaTask);
-    nuovaTask.innerText = testoTask.value;
-    elenco.append(bottoneRimuovi);
-    bottoneRimuovi.innerText = "-";
-    testoTask.value = "";
-  } else alert("Aggiungi del testo per aggiungere una task");
+    li.addEventListener("click", () => li.classList.toggle("completato"));
+
+    let rimuovi = document.createElement("button"); //credo il bottone rimuovi
+    li.append(rimuovi); //appendo a LI > button
+    rimuovi.innerText = "-"; //il testo del bottone diventa '-'
+
+    rimuovi.addEventListener("click", () => lista.removeChild(li));
+    //aggiungo evento che al click del bottone rimuovi toglie il filio LI di UL
+
+    testo.value = ""; //resetto input
+    testo.classList.remove("redplaceholder"); //rimuovo classe
+    testo.placeholder = "Ricordami di..."; //resetto placeholder nel caso in cui avessi tentato di inserire una task vuota
+  } else {
+    testo.classList.add("redplaceholder"); //aggiungo classe a input per dare stile al placeholder nel caso in cui non si inserisca alcun testo nell'input
+    testo.placeholder = "Devi inserire del testo"; //rimpiazzio il placeholder
+  }
 }
-
-function completed() {}
-
-function removeTask() {}
