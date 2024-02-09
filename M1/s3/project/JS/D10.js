4e14b039533b5dea1a92795bde944daebd1e3ae9
iosonodipe <dipecod@gmail.com>
2024-02-09 16:57:17 +0100

    extra

--- M1/s3/project/JS/D10.js
+++ M1/s3/project/JS/D10.js
@@ -381,7 +381,9 @@ let sumAllTheYears = () =>
 */
 
 function searchByTitle(stringa) {
-  let filmMatch = movies.filter((film) => film.Title.toLowerCase().includes(stringa));
+  let filmMatch = movies.filter((film) =>
+    film.Title.toLowerCase().includes(stringa)
+  );
   return filmMatch;
 }
 
@@ -484,20 +486,27 @@ let aggiungiClasse = () => {
 
 */
 
+function halfTree(numero) {
+  let asterisco = "";
+  for (let i = 0; i < numero; i++) {
+    asterisco += "*";
+    console.log(asterisco);
+  }
+}
+
 /* ESERCIZIO 28
-  Crea una funzione chiamata "tree" che riceve un numero come parametro e costruisce un albero di "*" (asterischi) dell'altezza fornita.
+Crea una funzione chiamata "tree" che riceve un numero come parametro e costruisce un albero di "*" (asterischi) dell'altezza fornita.
 
-  Esempio:
-  tree(3)
+Esempio:
+tree(3)
 
-    *
-   ***
-  *****
+*
+***
+*****
 
 */
+
 
 /* ESERCIZIO 29
   Crea una funzione chiamata "isItPrime" che riceve un numero come parametro e ritorna true se il numero fornito è un numero primo.
 */
-
-/* Questo array viene usato per gli esercizi. Non modificarlo. */
