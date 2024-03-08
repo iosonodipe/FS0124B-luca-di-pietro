c49c56a0ae546b6a1fa658b3a00eebae70a565a0
iosonodipe <dipecod@gmail.com>
2024-03-08 12:24:43 +0100

    aggiungi.js e riempimento prodotti su index

---
+++ M2/s3/project/js/aggiungi.js
@@ -0,0 +1,50 @@
+class Product {
+    constructor(_name, _description, _brand, _imageUrl, _price) {
+        this.name = _name;
+        this.description = _description;
+        this.brand = _brand;
+        this.imageUrl = _imageUrl;
+        this.price = _price;
+    }
+}
+
+const addBtn = document.querySelector("#addBtn");
+addBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    addProduct();
+});
+
+function getInputValue(id) {
+    return document.querySelector(id).value;
+}
+
+async function addProduct() {
+    let productName = getInputValue("#productName");
+    let productBrand = getInputValue("#productBrand");
+    let productPrice = getInputValue("#productPrice");
+    let productImage = getInputValue("#productImage");
+    let productDesc = getInputValue("#productDesc");
+
+    let product = new Product(
+        productName,
+        productDesc,
+        productBrand,
+        productImage,
+        productPrice
+    );
+
+    fetch("https://striveschool-api.herokuapp.com/api/product/", {
+        method: "POST",
+        headers: {
+            Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWVhZTg4OTJkN2IxMTAwMTkwZTZmNWIiLCJpYXQiOjE3MDk4OTM3NjksImV4cCI6MTcxMTEwMzM2OX0.73fOiV3a4vGZ_L20YVP5v9_bPaJ30wE9c9MWHAUHXBU",
+            "Content-type": "application/json",
+        },
+        body: JSON.stringify(product)
+    })
+        .then((response) => response.json())
+        .then((res) => {
+            alert("Prodotto creato correttamente!");
+            //do conferma all'utente oppure lo mando via
+        });
+}
---
+++ M2/s3/project/js/index.js
@@ -0,0 +1,28 @@
+function clone() {
+    let template = document.querySelector("template");
+    let clone = template.content.cloneNode(true);
+    return clone;
+}
+
+fetch("https://striveschool-api.herokuapp.com/api/product/", {
+    method: "GET",
+    headers: {
+        Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWVhZTg4OTJkN2IxMTAwMTkwZTZmNWIiLCJpYXQiOjE3MDk4OTM3NjksImV4cCI6MTcxMTEwMzM2OX0.73fOiV3a4vGZ_L20YVP5v9_bPaJ30wE9c9MWHAUHXBU",
+        "Content-type": "application/json",
+    },
+})
+.then((response) => response.json())
+.then(prodotti => {
+    prodotti.forEach(prodotto => {
+        let card = clone();
+        let row = document.querySelector("#product-catalogue");
+        let productImage = card.querySelector('.card-img-top');
+        productImage.src = `${prodotto.imageUrl}`;
+        let productName = card.querySelector('.card-title');
+        productName.innerText = `${prodotto.name}`
+        let productDesc = card.querySelector('.card-text');
+        productDesc.innerText = `${prodotto.description}`
+        row.append(card)
+    });
+})
