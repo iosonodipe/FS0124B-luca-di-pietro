445af1599d694101c3b769726206795bef12f698
iosonodipe <dipecod@gmail.com>
2024-03-21 15:58:05 +0100

    m3s1g4 esercizio

--- M3/s1/g3/blog/src/app/app-routing.module.ts
+++ M3/s1/g3/blog/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InactivePostsComponent } from './pages/inactive-posts/inactive-posts.component';
+import { ActivePostsComponent } from './pages/active-posts/active-posts.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent
+  },
+  {
+    path:'active-posts',
+    component:  ActivePostsComponent
+  },
+  {
+    path:'inactive-posts',
+    component: InactivePostsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- M3/s1/g3/blog/src/app/app.module.ts
+++ M3/s1/g3/blog/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ActivePostsComponent } from './pages/active-posts/active-posts.component';
+import { InactivePostsComponent } from './pages/inactive-posts/inactive-posts.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ActivePostsComponent,
+    InactivePostsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
