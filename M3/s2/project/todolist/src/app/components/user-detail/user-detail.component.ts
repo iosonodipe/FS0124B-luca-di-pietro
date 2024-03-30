279d816553fd2d37f886a9163c012a0f6c4c71ab
iosonodipe <dipecod@gmail.com>
2024-03-30 18:41:54 +0100

    sezione utenti completa

--- M3/s2/project/todolist/src/app/components/user-detail/user-detail.component.ts
+++ M3/s2/project/todolist/src/app/components/user-detail/user-detail.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { iUser } from '../../models/iuser';
+import { iTodo } from '../../models/itodo';
+import { TodoService } from '../../services/todo.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-user-detail',
@@ -6,5 +10,15 @@ import { Component } from '@angular/core';
   styleUrl: './user-detail.component.scss'
 })
 export class UserDetailComponent {
+@Input() user!: iUser
+userTodosArray: iTodo[] = []
+
+constructor(private todoSvc: TodoService){}
+
+ngAfterContentInit(){
+  this.todoSvc.$getTodosArray
+  .pipe(map((todos) => todos.filter((todo) => todo.userId == this.user.id)))
+  .subscribe(todos => this.userTodosArray = todos);
+}
 
 }
