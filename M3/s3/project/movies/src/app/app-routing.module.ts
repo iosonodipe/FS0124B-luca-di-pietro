6050e8645a80adf902bc2e8627b3b159490cc9ed
iosonodipe <dipecod@gmail.com>
2024-04-06 18:32:11 +0200

    guards

--- M3/s3/project/movies/src/app/app-routing.module.ts
+++ M3/s3/project/movies/src/app/app-routing.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './pages/auth/auth.guard';
+import { GuestGuard } from './pages/auth/guest.guard';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () =>
       import('./pages/auth/auth.module').then((m) => m.AuthModule),
+    canActivate: [GuestGuard],
+    canActivateChild: [GuestGuard],
   },
   {
     path: 'profile',
     loadChildren: () =>
       import('./pages/profile/profile.module').then((m) => m.ProfileModule),
+    canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
   },
   {
     path: 'films-list',
@@ -18,11 +24,15 @@ const routes: Routes = [
       import('./pages/films-list/films-list.module').then(
         (m) => m.FilmsListModule
       ),
+    canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
   },
   {
     path: 'users',
     loadChildren: () =>
       import('./pages/users/users.module').then((m) => m.UsersModule),
+    canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
   },
 ];
 
--- M3/s3/project/movies/src/app/pages/auth/auth.guard.ts
+++ M3/s3/project/movies/src/app/pages/auth/auth.guard.ts
@@ -1,19 +1,38 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, GuardResult, MaybeAsync, RouterStateSnapshot } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanActivateChild,
+  GuardResult,
+  MaybeAsync,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate, CanActivateChild {
+  constructor(
+    private authSvc:AuthService,
+    private router:Router//per i redirect
+    ){}
+
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): MaybeAsync<GuardResult> {
-    return true;
+    state: RouterStateSnapshot
+  ): MaybeAsync<GuardResult> {
+    if (!this.authSvc.syncIsLoggedIn) {
+      this.router.navigate(['login']);
+    }
+
+    return this.authSvc.syncIsLoggedIn;
   }
   canActivateChild(
     childRoute: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): MaybeAsync<GuardResult> {
-    return true;
+    state: RouterStateSnapshot
+  ): MaybeAsync<GuardResult> {
+    return this.canActivate(childRoute, state);
   }
-  
 }
