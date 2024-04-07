22322bbb3d6b4b1241f83c43c4c862dfddc3674b
iosonodipe <dipecod@gmail.com>
2024-04-07 21:05:12 +0200

    avviso aggiunta film

--- M3/s3/project/movies/src/app/pages/add-film/add-film.component.ts
+++ M3/s3/project/movies/src/app/pages/add-film/add-film.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IFilm } from '../../models/i-film';
 import { FilmListService } from '../films-list/film-list.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-film',
@@ -9,11 +10,19 @@ import { FilmListService } from '../films-list/film-list.service';
 })
 export class AddFilmComponent {
 
-  constructor(private filmsSvc: FilmListService){}
+  constructor(private filmsSvc: FilmListService, private router: Router){}
 
   film: Partial<IFilm> = {'image': 'https://picsum.photos/200/300'}
+  isAdded: boolean = false
 
   addMovie(){
-    this.filmsSvc.addMovie(this.film).subscribe()
+    this.filmsSvc.addMovie(this.film).subscribe(() =>{
+      this.isAdded = true
+
+      setTimeout(()=> {
+        this.router.navigate(['films-list'])
+        this.isAdded = false
+      }, 2000)
+    })
   }
 }
